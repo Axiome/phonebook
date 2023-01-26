55988d8c118c4b24a90e0a0b5d59992c5be3da55
Axiome <n.libermann@gmail.com>
2023-01-26 15:49:08 +0100

    Connect to mondoDB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.static('build'))
@@ -31,60 +33,91 @@ let persons = [
 
 
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+        .then(persons => {
+          response.json(persons)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = persons.find(person =>  person.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+        .then(person => {
+          response.json(person)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+          response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
-  if (!body.name || !body.number){
-    return response.status(400).json({ error: 'name and number should be includ' })
-  }
 
-  const nameExist = persons.find(person => body.name === person.name)
-  if (nameExist) {
-    return response.status(400).json({ error: 'name must be unique' })
-  }
-
-
-  person = {
-    id: Math.floor(Math.random() * 666),
+  const person  = {
     name: body.name,
     number: body.number
   }
-  persons = persons.concat(person)
-  response.json(person)
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
+        .catch(error => net(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  // if (!body.name || !body.number){
+  //  return response.status(400).json({ error: 'name and number should be includ' })
+  // }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+        .then(savedPerson => {
+          response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  console.log(persons.length)
   const date = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}`)
-})
+  Person.count({}, (err, count) => {
+    response.send(`<p>Phonebook has info for ${count} people</p><p>${date}`)
+  })
 
+})
+////// ERROR MANAGEMENT //////
 const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'unknown endpoint'})
 }
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-const PORT = process.env.PORT || 3001
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+////// RUN SERVER //////
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`serveur runnin on port ${PORT}`)
 })
